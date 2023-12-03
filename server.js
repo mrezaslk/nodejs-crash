84f2f7358ae4b1258c911de460faad6c4555df55
mohammadreza salek <rezasalek75@gmail.com>
2023-12-03 18:12:23 +0330

    [wip]add route

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const express = require('express');
+const app = express();
+
+global.config = require('./config');
+
+app.use(express.static(__dirname + "/public"));
+
+// app.get('/', function (req, res) {
+//     console.log(req.query);
+//     res.send(`hello world ${req.query.username}`)
+// })
+app.get('/:username', function (req, res) {
+    console.log(req.params);
+    res.send(`hello `)
+})
+
+
+
+app.listen(config.port, () => {
+    console.log(`server is runnig on port ${config.port}`);
+})
