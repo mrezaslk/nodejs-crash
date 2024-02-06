42dc98e3bc9745a626a83899641608ce7df45f55
mohammadreza salek <rezasalek75@gmail.com>
2024-02-06 23:29:56 +0330

    [mod]fs(file system)module

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const os = require('os');
+// const os = require('os');
 
-let totalMemory = os.totalmem()
-let freeMemory = os.freemem()
+// let totalMemory = os.totalmem()
+// let freeMemory = os.freemem()
 
-console.log(`totalMemory ${totalMemory}`);
-console.log("freeMemory " + freeMemory);
+// console.log(`totalMemory ${totalMemory}`);
+// console.log("freeMemory " + freeMemory);
+
+
+
+const fs = require('fs');
+
+// const files = fs.readdirSync('./');
+
+// console.log("files", files);
+
+const filesAsync = fs.readdir('./', function (err, files) {
+    if (err) console.log("err", err);
+    else console.log("Result", files);
+})
