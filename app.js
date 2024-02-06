1071a6bccc665ee15fbff31524abac9ac75a4e89
mohammadreza salek <rezasalek75@gmail.com>
2024-02-06 23:46:41 +0330

    emmiter

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const EvenEmitter = require('events');
 const emitter = new EvenEmitter();
 
 // Register a listener
-emitter.on("messageLogged", function () {
-    console.log('Listener called');
+emitter.on("messageLogged", (arg) => {
+    console.log(arg.message);
 })
 //Raise an event 
-emitter.emit('messageLogged')
+emitter.emit('messageLogged', { id: 1, url: 'http:///', message: "here is the test" })
+
+
