79e15c317a48c87b715729ebd9957d3141e382fd
mohammadreza salek <rezasalek75@gmail.com>
2024-02-24 14:37:56 +0330

    put method

--- index.js
+++ index.js
@@ -45,27 +45,61 @@ app.get('/api/posts/:year/:month', (req, res) => {
 //     res.send(req.query);
 // })
 
+app.put('/api/courses/:id', (req, res) => {
+    // Look up the course
+    const course = courses.find((c => c.id === parseInt(req.params.id)))
+    if (!course) res.status(404).send('The course was not found')
 
-app.post('/api/courses', (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    });
-    const result = schema.validate(req.body)
-    if (result.error) {
+    // Validate
+    const result = validateCourse(req.body)
+    const { error } = validateCourse(req.body)
+    if (error) {
         res.status(400).send(result.error.details[0].message)
         return;
     }
+    // Update course
+    course.name = req.body.name
+    res.send(course)
 
 
+
+})
+
+
+
+
+app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body)
+    if (error) {
+        res.status(400).send(result.error.details[0].message)
+        return;
+    }
     const course = {
         id: courses.length + 1,
         name: req.body.name
     }
     courses.push(course);
     res.send(course);
-
-
 })
+
+
+
+
+
+
+
+
+
+
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    });
+    return schema.validate(course)
+}
+
+
 
 
 
